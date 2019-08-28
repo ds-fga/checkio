346f3c556e44ecc02cd3eb227173cd1659d16561
caeslucio <ccaetanoo+github@gmail.com>
2019-08-28 17:08:21 -0300

    Create say-hi.js
    Disponibilização de duas soluções

---
+++ elementary/say-hi.js
@@ -0,0 +1,9 @@
+//Usando um tipo de sintaxe
+function sayHi(name, age) {
+   return `Hi. My name is ${name} and I'm ${age} years old`;
+}
+
+//Usando outro tipo de suntaxe
+function sayHi(name, age) {
+   return "Hi. My name is " + name + " and I'm " + age + " years old";
+}
